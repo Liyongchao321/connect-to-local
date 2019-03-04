0dbc9711f3424a7d2d49e16e24c97388fe14272c
Liyongchao321 <axidada@outlook.com>
2019-03-05 01:10:14 +0800

    @Alex modify config

--- AKStest/config.py
+++ AKStest/config.py
@@ -1,5 +1,1 @@
-appId ="dae41d90-bcc1-40b4-94ee-d1d58852383f",
-displayName = "azure-cli-2019-01-18-12-34-13",
-name = "http://azure-cli-2019-01-18-12-34-13",
-password = "e8120bf0-b7fa-46ee-8df1-cb79362c982a",
-tenant = "06b80a05-d0cc-47a4-ab85-4b4a0a1248ca"
+
